7116d408540f3796b2e27207e3e5331f232d16e2
Quizball <quizball2019@gmail.com>
2019-05-22 18:49:58 +0200

    Działa

--- app/src/main/java/com/example/myapplication/FragmentSingle.java
+++ app/src/main/java/com/example/myapplication/FragmentSingle.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class FragmentSingle extends Fragment {
+
+public class FragmentSingle extends Fragment{
     public FragmentSingle() {
     }
     View view;
@@ -17,6 +20,22 @@ public class FragmentSingle extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
       view=inflater.inflate(R.layout.single_fragment,container,false);
+
+
+        Button button = (Button) view.findViewById(R.id.button_quiz);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startQuiz();
+            }
+        });
         return view;
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(getActivity(), com.example.myapplication.QuizActivity.class);
+        startActivity(intent);
     }
 }
